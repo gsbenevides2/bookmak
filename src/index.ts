0e9c243eed342ac7e626906c1d1b9c64c1ce30d5
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2024-03-07 08:47:16 -0300

    feat: initial settings usign express and ejs

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-console.log("Welcome to Bookmark!");
-console.log("A ecommerce website for books.");
+import express from "express";
+import "reflect-metadata";
+
+const server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static("public"));
+server.set("view engine", "ejs");
+
+const serverPort = process.env.PORT ?? 3000;
+
+server.get("/", (_req, res) => {
+  res.render("index");
+});
+
+server.listen(serverPort, () => {
+  console.log(`Server is running at http://localhost:${serverPort}`);
+});
