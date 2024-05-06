2d9f7663e2d162a5a73c1ba57ab0ac8c0ce08f18
Guilherme da Silva Benevides <git@gui.dev.br>
2024-05-06 18:13:53 -0300

    temp: comment for presentation

--- cypress/e2e/index.spec.cy.ts
+++ cypress/e2e/index.spec.cy.ts
@@ -4,7 +4,7 @@ import "./cancel/index.spec.cy";
 import "./cart/index.spec.cy";
 import "./change/index.spec.cy";
 import "./checkoutAddresses/index.spec.cy";
-import "./clientCrud/index.spec.cy";
+//import "./clientCrud/index.spec.cy";
 import "./home/index.spec.cy";
 import "./payment/index.spec.cy";
 import "./product/index.spec.cy";
